bd9577102acc665e30b9729dad54924b10abd9c3
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2023-01-25 00:57:43 +0800

    JS Lesson 17 - Objects

--- week5/commits.js
+++ week5/commits.js
@@ -1,10 +1,18 @@
 function get_commit_info() {
+  return {
+    name: "Commit 1",
+    info: "Placeholder details about commit"
+  };
+}
+
+function generate_html() {
+  commit_obj = get_commit_info();
   newHTML =
-  `
-  <h3>Commit 1</h3>
-  <p>Placeholder details about commit</p>
-  `;
+    `
+    <h3>${commit_obj.name}</h3>
+    <p>${commit_obj.info}</p>
+    `;
   return newHTML;
 }
 
-document.getElementById("commits").innerHTML = get_commit_info();
+document.getElementById("commits").innerHTML = generate_html();
