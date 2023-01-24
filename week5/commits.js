c9f2e2e670dd5e26bd872fc9ac385a3ffcc637b9
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2023-01-25 01:04:38 +0800

    JS Lesson 10 - Loops & Iterables

--- week5/commits.js
+++ week5/commits.js
@@ -1,17 +1,30 @@
 function get_commit_info() {
-  return {
-    name: "Commit 1",
-    info: "Placeholder details about commit"
-  };
+  return [
+    {
+      name: "Commit 1",
+      info: "Placeholder details about commit 1"
+    },
+    {
+      name: "Commit 2",
+      info: "Placeholder details about commit 2"
+    },
+    {
+      name: "Commit 3",
+      info: "Placeholder details about commit 3"
+    }
+  ];
 }
 
 function generate_html() {
-  commit_obj = get_commit_info();
-  newHTML =
-    `
-    <h3>${commit_obj.name}</h3>
-    <p>${commit_obj.info}</p>
-    `;
+  commits = get_commit_info();
+  newHTML = ""
+  for (commit of commits) {
+    newHTML +=
+      `
+      <h3>${commit.name}</h3>
+      <p>${commit.info}</p>
+      `;
+  }
   return newHTML;
 }
 
