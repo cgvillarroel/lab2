21a57277f9287c2d0716588194f6dcacac6113b7
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2023-01-25 00:51:12 +0800

    JS Lesson 18 - Functions

--- week5/commits.js
+++ week5/commits.js
@@ -1,7 +1,10 @@
-newHTML =
-`
-<h3>Commit 1</h3>
-<p>Placeholder details about commit</p>
-`;
+function get_commit_info() {
+  newHTML =
+  `
+  <h3>Commit 1</h3>
+  <p>Placeholder details about commit</p>
+  `;
+  return newHTML;
+}
 
-document.getElementById("commits").innerHTML = newHTML;
+document.getElementById("commits").innerHTML = get_commit_info();
